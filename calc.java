7a2b143a9e2af75c50b033394b3bcdadb935cd1b
Brack <brackallen5271@gmail.com>
2026-08-24 15:48:16 -0500

    pretty much done with the calculator. I added multiplication and division, and also changed the input to be a single line equation instead of separate inputs for each number and operator.

--- calc.java
+++ calc.java
@@ -2,22 +2,34 @@ import java.util.Scanner;
 
 public class calc {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);   // Create Scanner
-
-        System.out.print("Enter Operator: ");       // Get Operator
-        String op = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);  // Create Scanner
         
-        if (op == "+") {
-            System.out.println("You want to add");
+        System.out.print("Two Number Calculator\nEnter Equation: ");
+        String equation = scanner.nextLine();
+
+        String[] parts = equation.split(" ");
+
+        double num1 = Double.parseDouble(parts[0]);
+        String op = parts[1];
+        double num2 = Double.parseDouble(parts[2]);
+
+        switch (op) {
+            case "+":
+                System.out.println("Sum: " + (num1 + num2));
+                break;
+            case "-":
+                System.out.println("Difference: " + (num1 - num2));
+                break;
+            case "*":
+                System.out.println("Product: " + (num1 * num2));
+                break;
+            case "/":
+                System.out.println("Quotient: " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid Operator");
         }
-        else if (op.equals("-")) {
-            System.out.println("You want to subtract");
-        }
-        else if (op == "*") {
-            System.out.println("You want to multiply");
-        }
-        else if (op == "/") {
-            System.out.println("You want to divide");
-        }
+
+        scanner.close();
     }
 }
